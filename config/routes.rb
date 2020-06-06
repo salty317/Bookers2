e737715103c0ce67912b41d76b694b6ee6eded2d
Shion Tanaka <b8219@oic.jp>
2020-06-06 22:43:27 +0900

    [Update] home#aboutを名前付きパスに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/about'
+  get 'home/about' => 'home#about', as: 'about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#top'
