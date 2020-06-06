085c2a9709a5fbce6c5abae74920a22f48531e23
Shion Tanaka <b8219@oic.jp>
2020-06-07 03:58:39 +0900

    [Update] UserInfo用の関数を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :current_user_info
 
   def index
     @users = User.all
@@ -11,5 +11,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  private
+  def current_user_info
+    @user = User.find(current_user.id)
   end
 end
