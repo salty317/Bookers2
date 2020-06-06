fe8a2c5dc7a4215fe7d73fc1c6353b2d6b64e46e
Shion Tanaka <b8219@oic.jp>
2020-06-07 00:45:54 +0900

    [Update] usersコントローラのRESTfulなルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'home#top'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   get 'home/about' => 'home#about', as: 'about'
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
