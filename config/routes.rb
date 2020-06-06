f0982bc1ed91d33f007f865f04ca6963b7363fbe
Shion Tanaka <b8219@oic.jp>
2020-06-06 22:39:07 +0900

    [Update] rootパスをhome#topに指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top'
   get 'home/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#top'
 end
