d359165c133f563367d0369b544a74aed98726cd
Shion Tanaka <b8219@oic.jp>
2020-06-11 03:36:41 +0900

    [Update] Booksコントローラーの各アクションにインスタンス変数を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,15 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
